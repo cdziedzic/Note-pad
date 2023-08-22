432200b99d414258586a4e3cb528dc03c2bcb88f
chris dziedzic <cdziedzic@gmail.com>
2023-08-21 21:12:14 -0400

    added get for index/notes html & api get

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3001;
+const notes = require('./db/db.json')
+
+app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+   return res.json(notes)
+});
+
+// app.post('/api/notes' (req,res) => {
+
+// });
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
