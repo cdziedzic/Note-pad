7b827c6c54d4bdc0140454e8b6964c4df8e02d74
chris dziedzic <cdziedzic@gmail.com>
2023-08-21 23:17:43 -0400

    added save new note functionality, need to get existing note display working

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require('path');
+const notes = require('./db/db.json')
+const fs = require('fs')
 const app = express();
 const PORT = 3001;
-const notes = require('./db/db.json')
+const notesFilePath = path.join(__dirname, './db/db.json');
+
+class Note {
+    constructor(title, text, uuid){
+    this.title  = title
+    this.text = text
+    this.uuid = uuid
+}}
 
 app.use(express.static('public'));
 app.use(express.json())
@@ -17,12 +26,27 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/api/notes', (req, res) => {
-   return res.json(notes)
+    return res.json(notes)
 });
 
-// app.post('/api/notes' (req,res) => {
 
-// });
+
+  
+    
+
+app.post('/api/notes', (req, res) => {
+  
+  function createNewNote() {
+    const newNote = new Note (`${req.body.title}`, `${req.body.text}`, `uuid`)
+    if (!Array.isArray(notes))
+        notes = [];
+      notes.push(newNote)
+    fs.writeFileSync(notesFilePath, JSON.stringify(notes))
+  }
+   
+    createNewNote()
+    getNotes()
+});
 
 
 app.listen(PORT, () =>
